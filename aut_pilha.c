b4af497c8e521cb4a6df63a26a3b3bef316bee15
ValneyFaria <valneyfaria@hotmail.com>
2018-06-28 16:45:20 -0300

    A

--- aut_pilha.c
+++ aut_pilha.c
@@ -1,64 +1,55 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "aut_pilha.h"
-//Este autômato de pilha reconhece linguagens do tipo {0^n1^n/n>0}
+// Este autômato de pilha reconhece linguagens do tipo {0^n1^n/n>0}
 
-//Função para criar a pilha
-Pilha *cria(void)
-{
+// Função para criar a pilha
+Pilha *cria(void) {
 	Pilha *p = (Pilha *)malloc(sizeof(Pilha));
 	p->prim = NULL;
 	return p;
 }
 
-//Função auxiliar para inserir no início
-No *ins_ini(No *l, int v)
-{
+// Função auxiliar para inserir no início
+No *ins_ini(No *l, int v) {
 	No *p = (No *)malloc(sizeof(No));
 	p->info = v;
 	p->prox = l;
 	return p;
 }
 
-//Função auxiliar para retirar do início
-No *ret_ini(No *l)
-{
+// Função auxiliar para retirar do início
+No *ret_ini(No *l) {
 	No *p = l->prox;
 	free(l);
 	return (p);
 }
 
-//Inserção na PILHA: int v é o dado do Nó
-void push(Pilha *p, int v)
-{
+// Inserção na PILHA: int v é o dado do Nó
+void push(Pilha *p, int v) {
 	p->prim = ins_ini(p->prim, v);
 }
 
-//Função de remoção da pilha
-int pop(Pilha *p)
-{
+// Função de remoção da pilha
+int pop(Pilha *p) {
 	int v;
-	if (vazia(p))
-	{
+	if (vazia(p)) {
 		printf("Pilha vazia!\n");
-		exit(1); //se a pilha está vazia, sai do programa
+		exit(1); // se a pilha está vazia, sai do programa
 	}
 	v = p->prim->info;
 	p->prim = ret_ini(p->prim);
 	return v;
 }
 
-//Função auxiliar para verificar se a pilha está vazia
-int vazia(Pilha *p)
-{
+// Função auxiliar para verificar se a pilha está vazia
+int vazia(Pilha *p) {
 	return (p->prim == NULL);
 }
 
-void libera(Pilha *p)
-{
+void libera(Pilha *p) {
 	No *r = p->prim;
-	while (r != NULL)
-	{
+	while (r != NULL) {
 		No *t = r->prox;
 		free(r);
 		r = t;
@@ -66,63 +57,59 @@ void libera(Pilha *p)
 	free(p);
 }
 
-//Função que exibe todos elementos da pilha
-void imprimePilha(Pilha *p)
-{
+// Função que exibe todos elementos da pilha
+void imprimePilha(Pilha *p) {
 	No *r = p->prim;
 	printf("Pilha:\n");
 	if (r == NULL)
 		printf("A pilha está vazia!");
-	while (r != NULL)
-	{
+	while (r != NULL) {
 		printf(".: |%d| :.\n", r->info);
 		r = r->prox;
 	}
 }
 
-int RECONHECE_PAR(Pilha *pil)
-{
+int RECONHECE_PAR(Pilha *pil) {
 	int q = 0, teste = 0;
 	printf("Cadeia a ser processada:\n");
 	for (i = 0; i < n; i++)
 		printf("%d", cadeia[i]);
 	printf("\nElemento do topo da pilha:\n%d\nEstado do autômato: %d\n\n", dado, q);
-	for (i = 0; i < n; i++)
-	{
+	for (i = 0; i < n; i++) {
+		if (cadeia[i] == 2) {
+			teste = 1;
+			return teste;
+		}
+
 		if (q == -1)
 			break;
 		printf("Processando %d elemento da cadeia: %d\n", i + 1, cadeia[i]);
-		if (cadeia[i] == 0 && pil->prim->info == 0 && q == 0)
-		{
+		if (cadeia[i] == 0 && pil->prim->info == 0 && q == 0) {
 			dado = pop(pil);
 			push(pil, 0);
 			push(pil, 1);
 			imprimePilha(pil);
 			printf("Estado do autômato: %d\n\n", q);
 		}
-		else if (cadeia[i] == 0 && pil->prim->info == 1 && q == 0)
-		{
+		else if (cadeia[i] == 0 && pil->prim->info == 1 && q == 0) {
 			dado = pop(pil);
 			push(pil, 1);
 			push(pil, 1);
 			imprimePilha(pil);
 			printf("Estado do autômato: %d\n\n", q);
 		}
-		else if (cadeia[i] == 1 && pil->prim->info == 1 && q == 0)
-		{
+		else if (cadeia[i] == 1 && pil->prim->info == 1 && q == 0) {
 			q = 1;
 			dado = pop(pil);
 			imprimePilha(pil);
 			printf("Estado do autômato: %d\n\n", q);
 		}
-		else if (cadeia[i] == 1 && pil->prim->info == 1 && q == 1)
-		{
+		else if (cadeia[i] == 1 && pil->prim->info == 1 && q == 1) {
 			dado = pop(pil);
 			imprimePilha(pil);
 			printf("Estado do autômato: %d\n\n", q);
 		}
-		else
-		{
+		else {
 			q = -1;
 			imprimePilha(pil);
 			printf("Estado final!\n");
@@ -133,27 +120,22 @@ int RECONHECE_PAR(Pilha *pil)
 	printf("Fim do reconhecimento da cadeia!\n");
 	if (q == -1)
 		return teste;
-	else
-	{
+	else {
 		printf("\nProcessando pilha\n");
-		while (pil->prim != NULL)
-		{
-			if (q == 0 && pil->prim->info == 0)
-			{
+		while (pil->prim != NULL) {
+			if (q == 0 && pil->prim->info == 0) {
 				q = 2;
 				dado = pop(pil);
 				imprimePilha(pil);
 				printf("Estado do autômato: %d\n\n", q);
 			}
-			else if (q == 1 && pil->prim->info == 0)
-			{
+			else if (q == 1 && pil->prim->info == 0) {
 				q = 2;
 				dado = pop(pil);
 				imprimePilha(pil);
 				printf("Estado do autômato: %d\n\n", q);
 			}
-			else
-			{
+			else {
 				q = -1;
 				dado = pop(pil);
 				imprimePilha(pil);
@@ -168,20 +150,31 @@ int RECONHECE_PAR(Pilha *pil)
 	}
 }
 
-void inicio()
-{
-	printf("Insira o tamanho da sequência\n");
-	scanf("%d", &n);
+void inicio() {	
+	int flag = 1;
+
+	printf("Insira o tamanho da sequência:\n");
+	while (flag) {
+		scanf("%d", &n);
+
+		if (n > 0) {
+			flag = 0;
+		}
+		else {
+			printf("Valor Inválido!\n");
+		}
+	}
+	
 	system("clear");
 	cadeia = (int *)malloc(n * sizeof(int));
 
-	printf("Insira a sequência:\n");
-	for (i = 0; i < n; i++)
-	{
+	//  Simbolo de Palavra Vazia : 2
+	printf("Insira a sequência de entrada(2: palavra vazia):\n");
+	for (i = 0; i < n; i++) {
 		scanf("%d", &cadeia[i]);
 	}
-	system("clear");
+	// system("clear");
 	printf("Insira o primeiro elemento da lista:\n");
 	scanf("%d\n", &dado);
-	system("clear");
+	// system("clear");
 }
